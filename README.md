33f62009b9fed893b0fb26da5004f6b9002d4460
Huffer342-WSH <718007138@qq.com>
2025-04-04 15:28:36 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# 适用于OJ做题的C++ VSCode工作区模板
+
+## 使用方法
+1. 编写C++代码，仅限单文件
+2. 撰写输入到`io/in.txt`，作为stdin
+3. `Terminal -> Run Build Task(ctrl+shift+B)` 编译并运行代码
+4. 在终端或者`io/out.txt`查看输出（stdout）
---
+++ demo.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+    string s;
+    cout << "Hello world" << endl;
+
+
+    string inputText((istreambuf_iterator<char>(cin)), istreambuf_iterator<char>());
+    cout << "Read content:\n" << inputText << endl;
+    return 0;
+}
